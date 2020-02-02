6d60c9a63a6575f57494ddb7fc0ec44bfeb145f4
Anthony <a.iacampo99@gmail.com>
2020-02-01 23:53:30 -0500

    Multithreading is now working!

--- Multithreading.java
+++ Multithreading.java
@@ -5,30 +5,19 @@ class Multithreading implements Runnable
 {
   static int[] bulbs;
   static int numBulbs;
+  int min;
+  int max;
   static int defectiveCounter;
   static int[] defectiveBulbs;
   static int threadCounter = 0;
 
   public static void main(String []args) throws Exception
   {
-    FileReader fr = new FileReader("Input.txt");
-    BufferedReader br = new BufferedReader(fr);
+    //Retrieve Input
+    readFile();
 
-    String input;
-    int argsIndex = 0;
-
-    numBulbs = Integer.parseInt(br.readLine());
-    bulbs = new int[numBulbs];
-    defectiveBulbs = new int[numBulbs];
-    
-    while((input = br.readLine()) != null)
-    {
-      bulbs[argsIndex] = Integer.parseInt(input);
-      argsIndex++;
-    }
-    br.close();
-
-    Thread t = new Thread(new Multithreading());
+    //Create initial thread
+    Thread t = new Thread(new Multithreading(0, bulbs.length));
     t.start();
     threadCounter++;
 
@@ -47,70 +36,91 @@ class Multithreading implements Runnable
     System.out.print("\nThe number of threads for this problem: " + threadCounter);
   }
 
+  //Runnable constructor
+  public Multithreading( int start, int end)
+  {
+    this.min = start;
+    this.max = end;
+  }
+    
+  //Read the input file
+  private static void readFile() throws Exception 
+  {
+    FileReader fr = new FileReader("Input.txt");
+    BufferedReader br = new BufferedReader(fr);
+    
+    String input;
+    int argsIndex = 0;
+    
+    numBulbs = Integer.parseInt(br.readLine());
+    bulbs = new int[numBulbs];
+    defectiveBulbs = new int[numBulbs];
+    while((input = br.readLine()) != null)
+    {
+      bulbs[argsIndex] = Integer.parseInt(input);
+      argsIndex++;
+    }
+    br.close();
+  }
+
   //Run thread
   public void run() 
   {
-    FindDefective(bulbs, 0 ,bulbs.length);
+    FindDefective();
   }
 
   //Finds the defective bulb
-  public void FindDefective(int[] bulbs, int min, int max)
+  public void FindDefective()
   {
-      if(max - min == 1)
+    int pivot = (min + max) / 2;
+    if(max - min == 1)
       {
-        defectiveBulbs[defectiveCounter] = max;
-        defectiveCounter++;
-        return;
-      }
-
-      int pivot = (min + max) / 2;
-      boolean isLeftDefective = false;
-      boolean isRightDefective = false;
-    
-      //Left array
-      Thread left = new Thread();
-      threadCounter++;
-      left.start();
-      for(int i = min; i < pivot; i++)
-      {
-       if(bulbs[i] == 0)
+        if(bulbs[min] == 0)
         {
-          isLeftDefective = true;
+          defectiveBulbs[defectiveCounter] = max;
+          defectiveCounter++;
+          return;
         }
-      }
-     if(isLeftDefective)
-      {
-        FindDefective(bulbs, min, pivot);
-      }
-
-      //Right array
-      Thread right = new Thread();
-      threadCounter++;
-      right.start();
-
-      for(int i = pivot; i < max; i++)
-      {
-       if(bulbs[i] == 0)
-       {
-         isRightDefective = true;
-       }
-      }
-      if(isRightDefective)
-      {
-       FindDefective(bulbs, pivot, max);
-      }
-
-    try 
-    {
-      left.join();
-      right.join();
-    }
-    catch(InterruptedException err)
-    {
-      System.out.println(err);
+      } else {
+        if(isDefective())
+        {
+          //System.out.println("left array"); //Just for test
+          Thread left = new Thread(new Multithreading(min, pivot));
+          threadCounter++;
+          left.start();
+    
+          //System.out.println("right array"); //Just for test
+          Thread right = new Thread(new Multithreading(pivot, max));
+          threadCounter++;
+          right.start();
+    
+          try 
+          {
+            left.join();
+            right.join();
+          }
+          catch(InterruptedException err)
+          {
+            System.out.println(err);
+          }
+        }
     }
   }
 
+  //Returns true if a defective bulb is found
+  public boolean isDefective()
+  {
+    for(int i = min; i < max; i++ )
+    {
+      if(bulbs[i] == 0)
+      {
+				return true;
+			}
+		}
+		return false;
+  }
+
+  //Prints the array of indexes of defective bulb
   private static void printArray(int[] array)
   {
     for(int i = 0; i < array.length; i++)
